0397bf8299430ab93d693076dc750a4f0a3b64d9
pirateboy7654 <82248187+pirateboy7654@users.noreply.github.com>
2024-10-24 15:51:52 -0700

    project 1

---
+++ Project 1/command.c
@@ -0,0 +1,179 @@
+#include <dirent.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <sys/syscall.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+
+
+/*for the ls command*/
+void listDir() {
+    //printf("ls command\n");
+    DIR *d;
+    struct dirent *dir;
+
+    // open the current directory
+    d = opendir(".");
+    if (d == NULL) { // error check if cant open
+        perror("opendir");
+        return;
+    }
+
+    // read directory entries
+    while ((dir = readdir(d)) != NULL) {
+        // print file name
+        if (dir->d_name[0] != '.') {  // ignores hidden files
+            write(STDOUT_FILENO, dir->d_name, strlen(dir->d_name));
+            write(STDOUT_FILENO, "\n", 1);
+        }
+    }
+
+    // close directory with error check
+    if (closedir(d) == -1) {
+        perror("closedir");
+    }
+}
+
+/*for the pwd command*/
+void showCurrentDir() {
+    size_t size = 1024;  // initial buffer size
+    char *buffer;  // initial buffer for cwd
+
+    // allocate memory for the buffer
+    buffer = (char *)malloc(size);
+    if (buffer == NULL) {
+        perror("Unable to allocate buffer");
+        return;
+    }
+
+    // get the cwd
+    if (getcwd(buffer, size) != NULL) {
+        // output the cwd
+        write(STDOUT_FILENO, buffer, strlen(buffer));
+        write(STDOUT_FILENO, "\n", 1); // newline
+    } else {
+        // error if getcwd() fails
+        perror("getcwd() error");
+    }
+
+    // free the buffer
+    free(buffer);
+}
+
+/*for the mkdir command*/
+void makeDir(char *dirName) {
+    //printf("mkdir command\n");
+    // creates a dir with r/w/x perms
+    if (mkdir(dirName, 0755) == -1) {
+        perror("make dir");
+    }
+}
+/*for the cd command*/
+void changeDir(char *dirName) {
+    //printf("cd command\n");
+    // changes dir to dirname
+    if (chdir(dirName) == -1) {
+        perror("change dir");
+    }
+}
+
+/*for the cp command*/
+void copyFile(char *sourcePath, char *destinationPath) {
+    //printf("cp command\n");
+    int src, dst;
+    char buffer[1024];
+    ssize_t bytes_read;
+
+    
+
+    // open src file for reading
+    src = open(sourcePath, O_RDONLY);
+    if (src == -1) {
+        perror("open (source)"); // error if cant open src file
+        return;
+    }
+
+    // handle case where dst path is '.' ie cant write to a directory need to write to a file
+    char full_dest_path[1024];
+    char *src_name = strrchr(sourcePath, '/'); // gets file name
+    if (strcmp(destinationPath, ".") == 0) {
+        
+        strcpy(full_dest_path, ".");  // copy "./" to the dest path
+        strcat(full_dest_path, src_name);  // append file name to "./"
+    } 
+    else {
+        strcpy(full_dest_path, destinationPath); // copy the destination path
+    }
+    
+
+    // open dst file for writing (create if doesn't exist)
+    dst = open(full_dest_path, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+    if (dst == -1) {
+        perror("open (destination)"); // error if cant open dst file
+        close(src);
+        return;
+    }
+
+    // copy file contents from src to dst
+    while ((bytes_read = read(src, buffer, sizeof(buffer))) > 0) {
+        if (write(dst, buffer, bytes_read) != bytes_read) {
+            perror("write"); // error if cant copy
+            close(src);
+            close(dst);
+            return;
+        }
+    }
+
+    // close files
+    close(src);
+    close(dst);
+}
+
+/*for the mv command*/
+void moveFile(char *sourcePath, char *destinationPath) {
+    //printf("mv command\n");
+    // moves or renames the file
+    if (rename(sourcePath, destinationPath) == -1) {
+        perror("rename");
+    }
+}
+
+/*for the rm command*/
+void deleteFile(char *filename) {
+    //printf("rm command\n");
+    // deletes the file
+    if (remove(filename) == -1) {
+        perror("remove");
+    }
+}
+
+/*for the cat command*/
+void displayFile(char *filename) {
+    //printf("cat command\n");
+    int file_des;
+    char buffer[1024];
+    ssize_t bytes_read;
+
+    // open the file for reading
+    file_des = open(filename, O_RDONLY);
+    if (file_des == -1) {
+        perror("open");
+        return;
+    }
+
+    // Read and display the file's contents
+    while ((bytes_read = read(file_des, buffer, sizeof(buffer))) > 0) {
+        if (write(STDOUT_FILENO, buffer, bytes_read) != bytes_read) {
+            perror("write");
+            close(file_des);
+            return;
+        }
+    }
+
+    // Close the file
+    close(file_des);
+}
---
+++ Project 1/main.c
@@ -0,0 +1,387 @@
+/*
+main.c must be able to start in both of the following modes:
+a. File mode:
+    i. 
+        File mode is called by using the -f flag and passing it a filename
+        (e.g. pseudo-shell -f <filename>)
+    ii. 
+        The shell only accepts filenames in file mode.
+    iii.
+        All input is taken from the file whose name is specified by <filename>
+        and all output is written to the file “output.txt”. There should be no
+        console output in this mode. Place all output in the file.
+b. Interactive mode:
+    i. 
+        If no command line argument is given the project will start in interactive
+        mode.
+    ii. 
+        The program accepts input from the console and writes output from the
+        console.
+*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <dirent.h>
+#include "command.h"
+#include "string_parser.h"
+
+//function declarations
+void interactive_mode();
+void file_mode(const char *filename);
+
+int main(int argc, char const *argv[])
+{
+    // identify which mode to open in
+    // no arguments = interactive mode
+    if (argc == 1) {
+        interactive_mode();
+    }
+    // -f <filename> = file mode
+    else if ((argc == 3) && (strcmp(argv[1], "-f") == 0)) {
+        file_mode(argv[2]);
+    }
+    //error check for improper arguments
+    else {
+        fprintf(stderr, "Error Improper Usage: \n"
+                "File Mode: pseudo-shell -f <filename> \n"
+                "Interactive Mode: pseudo-shell\n");
+        exit(EXIT_FAILURE);
+    }
+    return 0;
+}
+
+void interactive_mode() {
+    char *line = NULL;
+    size_t len = 0;
+
+    printf(">>> ");  // Prompt for user input
+
+    while (getline(&line, &len, stdin) != -1) {
+        // strip newline character
+        line[strcspn(line, "\n")] = 0; 
+        //printf("you wrote: %s\n", line);
+
+        // check if user wants to exit
+        if (strcmp(line, "exit") == 0) {
+            break;
+        }
+
+        // Parse and execute the command
+        // use strtok() to parse the line here and call the relevant function
+        // For example:
+        // parseAndExecute(line);
+
+        // copied from lab 1
+        
+
+        command_line large_token_buffer;
+        command_line small_token_buffer;
+
+        large_token_buffer = str_filler (line, ";");
+
+        for (int i = 0; large_token_buffer.command_list[i] != NULL; i++)
+		{
+            // prints large token number
+			//printf ("\tLine segment %d:\n", i + 1);
+
+			//tokenize large buffer
+			//smaller token is seperated by " "(space bar)
+			small_token_buffer = str_filler (large_token_buffer.command_list[i], " ");
+
+			//iterate through each smaller token to print
+            //printf("\t\tToken 1: %s, # of tokens: %d", small_token_buffer.command_list[0], small_token_buffer.num_token);
+			/*for (int j = 0; small_token_buffer.command_list[j] != NULL; j++)
+			{
+				printf ("\t\tToken %d: %s\n", j + 1, small_token_buffer.command_list[j]);
+			}*/
+
+            // identify the command and error check, then preform command
+            if (small_token_buffer.command_list[0] != NULL) {
+                // execute the command based on the first token
+
+                // exit
+                if ((strcmp(small_token_buffer.command_list[0], "exit") == 0)) {
+                    // error checking token # 2 (in position 1) != null
+                    if (small_token_buffer.command_list[1] != NULL) {
+                        printf("Error! Unsupported parameters for command: %s\n", small_token_buffer.command_list[0]);
+                    }
+                    /*
+                    printf("compared token: %s\n", small_token_buffer.command_list[1]);
+                    printf("number of tokens: %d\n", small_token_buffer.num_token);
+                    */
+                    else {
+                    free_command_line (&large_token_buffer);
+                    free_command_line(&small_token_buffer);
+                    free(line);  
+                    return; }
+                }
+                // ls 
+                else if (strcmp(small_token_buffer.command_list[0], "ls") == 0) {
+                    // error checking token # 2 (in position 1) != null
+                    if (small_token_buffer.command_list[1] != NULL) {
+                        printf("Error! Unsupported parameters for command: %s\n", small_token_buffer.command_list[0]);
+                    }
+                    else { listDir();}
+
+                }
+                // pwd
+                else if (strcmp(small_token_buffer.command_list[0], "pwd") == 0) {
+                    // error checking token # 2 (in position 1) != null
+                    if (small_token_buffer.command_list[1] != NULL) {
+                        printf("Error! Unsupported parameters for command: %s\n", small_token_buffer.command_list[0]);
+                    }
+                    else {showCurrentDir();  }// call function for "pwd"
+                } 
+                // mkdir
+                else if (strcmp(small_token_buffer.command_list[0], "mkdir") == 0) {
+                    // error checking token # 2 (in position 1) == null and # 3 != null
+                    if ((small_token_buffer.command_list[1] == NULL) || 
+                        small_token_buffer.command_list[2] != NULL) {
+                        printf("Error! Unsupported parameters for command: %s\n", small_token_buffer.command_list[0]);
+                    }
+                    else {makeDir(small_token_buffer.command_list[1]);}
+                }
+                // cd
+                else if (strcmp(small_token_buffer.command_list[0], "cd") == 0) {
+                    // error checking token # 2 (in position 1) == null and # 3 != null
+                    if ((small_token_buffer.command_list[1] == NULL) || 
+                        small_token_buffer.command_list[2] != NULL) {
+                        printf("Error! Unsupported parameters for command: %s\n", small_token_buffer.command_list[0]);
+                    }
+                    else {changeDir(small_token_buffer.command_list[1]);}
+                }
+                // cp
+                else if (strcmp(small_token_buffer.command_list[0], "cp") == 0) {
+                    // error checking token # 2 (in position 1) == null and # 3 == null and # 4 != null
+                    if ((small_token_buffer.command_list[1] == NULL) || 
+                        (small_token_buffer.command_list[2] == NULL) ||
+                        (small_token_buffer.command_list[3] != NULL)) {
+                        printf("Error! Unsupported parameters for command: %s\n", small_token_buffer.command_list[0]);
+                    }
+                    else {copyFile(small_token_buffer.command_list[1], small_token_buffer.command_list[2]);}
+                }
+                // mv
+                else if (strcmp(small_token_buffer.command_list[0], "mv") == 0) {
+                    // error checking token # 2 (in position 1) == null and # 3 == null and # 4 != null
+                    if ((small_token_buffer.command_list[1] == NULL) || 
+                        (small_token_buffer.command_list[2] == NULL) ||
+                        (small_token_buffer.command_list[3] != NULL)) {
+                        printf("Error! Unsupported parameters for command: %s\n", small_token_buffer.command_list[0]);
+                    }
+                    else {moveFile(small_token_buffer.command_list[1], small_token_buffer.command_list[2]);}
+                }
+                // rm
+                else if (strcmp(small_token_buffer.command_list[0], "rm") == 0) {
+                    // error checking token # 2 (in position 1) == null and # 3 != null
+                    if ((small_token_buffer.command_list[1] == NULL) || 
+                        small_token_buffer.command_list[2] != NULL) {
+                        printf("Error! Unsupported parameters for command: %s\n", small_token_buffer.command_list[0]);
+                    }
+                    else {deleteFile(small_token_buffer.command_list[1]);}
+                }
+                // cat
+                else if (strcmp(small_token_buffer.command_list[0], "cat") == 0) {
+                    // error checking token # 2 (in position 1) == null and # 3 != null
+                    if ((small_token_buffer.command_list[1] == NULL) || 
+                        small_token_buffer.command_list[2] != NULL) {
+                        printf("Error! Unsupported parameters for command: %s\n", small_token_buffer.command_list[0]);
+                    }
+                    else {displayFile(small_token_buffer.command_list[1]);}
+                }
+                // error unknown command
+                else {
+                    printf("Error! Unrecognized command: %s\n", small_token_buffer.command_list[0]);
+                }
+            }
+
+			//free smaller tokens and reset variable
+			free_command_line(&small_token_buffer);
+			memset (&small_token_buffer, 0, 0);
+		}
+
+		//free smaller tokens and reset variable
+		free_command_line (&large_token_buffer);
+		memset (&large_token_buffer, 0, 0);
+        // finish copy
+        printf(">>> ");  // prompt again after command execution
+    }
+
+    free(line);  // free the allocated memory for input line
+    return;
+}
+
+// mostly from lab 2 lfcat
+void file_mode(const char *filename) {
+    char *line = NULL;
+    size_t len = 0;
+
+    char cwd[1024];
+	getcwd(cwd, sizeof(cwd));
+
+    FILE *input_file = fopen(filename, "r");
+    if (input_file == NULL) {
+        perror("Error opening file");
+        return;
+    }
+
+    FILE *output_file = freopen("output.txt", "w", stdout); // redirect stdout to output file
+    freopen("output.txt", "w", stderr);  // redirect stderr to output file
+	if (output_file == NULL) {
+        perror("freopen");
+        fclose(input_file);
+        return;
+    }
+    while (getline(&line, &len, input_file) != -1) {
+        // strip newline character
+        line[strcspn(line, "\n")] = 0; 
+        //printf("you wrote: %s\n", line);
+
+        // check if user wants to exit
+        if (strcmp(line, "exit") == 0) {
+            break;
+        }
+
+        // Parse and execute the command
+        // use strtok() to parse the line here and call the relevant function
+        // For example:
+        // parseAndExecute(line);
+
+        // copied from lab 1
+        
+
+        command_line large_token_buffer;
+        command_line small_token_buffer;
+
+        large_token_buffer = str_filler (line, ";");
+
+        for (int i = 0; large_token_buffer.command_list[i] != NULL; i++)
+		{
+            // prints large token number
+			//printf ("\tLine segment %d:\n", i + 1);
+
+			//tokenize large buffer
+			//smaller token is seperated by " "(space bar)
+			small_token_buffer = str_filler (large_token_buffer.command_list[i], " ");
+
+			//iterate through each smaller token to print
+            //printf("\t\tToken 1: %s, # of tokens: %d", small_token_buffer.command_list[0], small_token_buffer.num_token);
+			/*for (int j = 0; small_token_buffer.command_list[j] != NULL; j++)
+			{
+				printf ("\t\tToken %d: %s\n", j + 1, small_token_buffer.command_list[j]);
+			}*/
+
+            // identify the command and error check, then preform command
+            if (small_token_buffer.command_list[0] != NULL) {
+                // execute the command based on the first token
+
+                // exit
+                if ((strcmp(small_token_buffer.command_list[0], "exit") == 0)) {
+                    // error checking token # 2 (in position 1) != null
+                    if (small_token_buffer.command_list[1] != NULL) {
+                        printf("Error! Unsupported parameters for command: %s\n", small_token_buffer.command_list[0]);
+                    }
+                    /*
+                    printf("compared token: %s\n", small_token_buffer.command_list[1]);
+                    printf("number of tokens: %d\n", small_token_buffer.num_token);
+                    */
+                    else {
+                    free_command_line (&large_token_buffer);
+                    free_command_line(&small_token_buffer);
+                    free(line);  
+                    return; }
+                }
+                // ls 
+                else if (strcmp(small_token_buffer.command_list[0], "ls") == 0) {
+                    // error checking token # 2 (in position 1) != null
+                    if (small_token_buffer.command_list[1] != NULL) {
+                        printf("Error! Unsupported parameters for command: %s\n", small_token_buffer.command_list[0]);
+                    }
+                    else { listDir();}
+
+                }
+                // pwd
+                else if (strcmp(small_token_buffer.command_list[0], "pwd") == 0) {
+                    // error checking token # 2 (in position 1) != null
+                    if (small_token_buffer.command_list[1] != NULL) {
+                        printf("Error! Unsupported parameters for command: %s\n", small_token_buffer.command_list[0]);
+                    }
+                    else {showCurrentDir();  }// call function for "pwd"
+                } 
+                // mkdir
+                else if (strcmp(small_token_buffer.command_list[0], "mkdir") == 0) {
+                    // error checking token # 2 (in position 1) == null and # 3 != null
+                    if ((small_token_buffer.command_list[1] == NULL) || 
+                        small_token_buffer.command_list[2] != NULL) {
+                        printf("Error! Unsupported parameters for command: %s\n", small_token_buffer.command_list[0]);
+                    }
+                    else {makeDir(small_token_buffer.command_list[1]);}
+                }
+                // cd
+                else if (strcmp(small_token_buffer.command_list[0], "cd") == 0) {
+                    // error checking token # 2 (in position 1) == null and # 3 != null
+                    if ((small_token_buffer.command_list[1] == NULL) || 
+                        small_token_buffer.command_list[2] != NULL) {
+                        printf("Error! Unsupported parameters for command: %s\n", small_token_buffer.command_list[0]);
+                    }
+                    else {changeDir(small_token_buffer.command_list[1]);}
+                }
+                // cp
+                else if (strcmp(small_token_buffer.command_list[0], "cp") == 0) {
+                    // error checking token # 2 (in position 1) == null and # 3 == null and # 4 != null
+                    if ((small_token_buffer.command_list[1] == NULL) || 
+                        (small_token_buffer.command_list[2] == NULL) ||
+                        (small_token_buffer.command_list[3] != NULL)) {
+                        printf("Error! Unsupported parameters for command: %s\n", small_token_buffer.command_list[0]);
+                    }
+                    else {copyFile(small_token_buffer.command_list[1], small_token_buffer.command_list[2]);}
+                }
+                // mv
+                else if (strcmp(small_token_buffer.command_list[0], "mv") == 0) {
+                    // error checking token # 2 (in position 1) == null and # 3 == null and # 4 != null
+                    if ((small_token_buffer.command_list[1] == NULL) || 
+                        (small_token_buffer.command_list[2] == NULL) ||
+                        (small_token_buffer.command_list[3] != NULL)) {
+                        printf("Error! Unsupported parameters for command: %s\n", small_token_buffer.command_list[0]);
+                    }
+                    else {moveFile(small_token_buffer.command_list[1], small_token_buffer.command_list[2]);}
+                }
+                // rm
+                else if (strcmp(small_token_buffer.command_list[0], "rm") == 0) {
+                    // error checking token # 2 (in position 1) == null and # 3 != null
+                    if ((small_token_buffer.command_list[1] == NULL) || 
+                        small_token_buffer.command_list[2] != NULL) {
+                        printf("Error! Unsupported parameters for command: %s\n", small_token_buffer.command_list[0]);
+                    }
+                    else {deleteFile(small_token_buffer.command_list[1]);}
+                }
+                // cat
+                else if (strcmp(small_token_buffer.command_list[0], "cat") == 0) {
+                    // error checking token # 2 (in position 1) == null and # 3 != null
+                    if ((small_token_buffer.command_list[1] == NULL) || 
+                        small_token_buffer.command_list[2] != NULL) {
+                        printf("Error! Unsupported parameters for command: %s\n", small_token_buffer.command_list[0]);
+                    }
+                    else {displayFile(small_token_buffer.command_list[1]);}
+                }
+                // error unknown command
+                else {
+                    printf("Error! Unrecognized command: %s\n", small_token_buffer.command_list[0]);
+                }
+            }
+
+			//free smaller tokens and reset variable
+			free_command_line(&small_token_buffer);
+			memset (&small_token_buffer, 0, 0);
+		}
+
+		//free smaller tokens and reset variable
+		free_command_line (&large_token_buffer);
+		memset (&large_token_buffer, 0, 0);
+        // finish copy
+        
+    }
+    fclose(stdout);
+    free(line);  // free the allocated memory for input line
+    return;
+}
